2d7e54627a23b18c2a1d549991d461ed07565436
johnwithsmile <john_with_smile@kakao.com>
2024-08-17 18:29:59 +0900

    js base

---
+++ javascript/02.js
@@ -0,0 +1,14 @@
+console.log("hello hello");
+
+let message;
+
+message = "Hello!";
+console.log(message);
+
+let secondMessage = "Hello again!";
+console.log(secondMessage);
+
+let user = "John";
+let age = 900;
+let welcomeMessage = "Hello";
+welcomeMessage = "World";
